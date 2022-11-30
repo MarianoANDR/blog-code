3a285382373a57c8fac28cc719cee2ada3120f71
MarianoANDR <mariano.uk2022@gmail.com>
2022-11-30 21:00:59 +0000

    commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import './App.css';
+
+function App() {
+  return (
+
+    <div className='bigDiv'>
+
+      <div className="img1Div">
+          
+      <img className='img' src='https://picsum.photos/612/408' alt='hands'></img>
+      <h4 className='h4Class'>Helping Hands</h4>
+      <h2 className='h2Class'>Helping hands are always welcome</h2>
+      <img className='avatarImg' src='https://www.w3schools.com/howto/img_avatar.png' alt='avatar'></img>
+      <span className='nameAv'>Graham Griffiths</span>
+    </div>
+
+
+    <div className="img2Div">
+      
+      <img className='img' src='https://media.istockphoto.com/id/872576234/photo/rescue.jpg?s=612x612&w=0&k=20&c=53Sskdnw4l3O_Wvx6sIcvveWwSxBxT1X-kkrZg-W9Cw=' alt='hands'></img>
+      <h4 className='h4Class'>Helping Hands</h4>
+      <h2 className='h2Class'>Helping hands are always welcome</h2>
+      <img className='avatarImg' src='https://www.w3schools.com/howto/img_avatar.png' alt='avatar'></img>
+      <span className='nameAv'>Graham Griffiths</span>
+    </div>
+
+    <div className="img3Div">
+      
+      <img className='img' src='https://media.istockphoto.com/id/1183967695/photo/be-the-person-who-helps-the-next.jpg?s=612x612&w=0&k=20&c=zUIWa_uRJ6GXU5VHpTezYt-C0w2C7zBLCFVVsDUZrX8=' alt='hands'></img>
+      <h4 className='h4Class'>Helping Hands</h4>
+      <h2 className='h2Class'>Helping hands are always welcome</h2>
+      <img className='avatarImg' src='https://www.w3schools.com/howto/img_avatar.png' alt='avatar'></img>
+      <span className='nameAv'>Graham Griffiths</span>
+    </div>
+    
+    </div>
+
+  );
+}
+
+export default App;
